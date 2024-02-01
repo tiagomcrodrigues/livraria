edd3822636da478f975e649221205e7db12e5b03
Tiago Rodrigues <tiagomcoutorodrigues@gmail.com>
2024-01-31 21:25:22 -0300

    criandoLivraria

---
+++ src/Livraria/Livraria/Data/Entities/Autores.cs
@@ -0,0 +1,19 @@
+﻿namespace Livraria.Data.Entities
+{
+    public class Autores
+    {
+
+        public Autores() { }
+
+        public Autores(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+        public string Nome{ get; set; }
+
+        public virtual ICollection<Livros> Livros { get; set; }
+    }
+}
